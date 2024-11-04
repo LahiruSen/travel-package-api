b8dc8e3b9f5e21aa5e663edeb05e2bf4a67544c9
LahiruSen <lahiru.16@cse.mrt.ac.lk>
2024-11-05 02:27:45 +1100

    initial commit

---
+++ src/main/java/com/lahiru/travelpackageapi/TravelPackageApiApplication.java
@@ -0,0 +1,13 @@
+package com.lahiru.travelpackageapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelPackageApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelPackageApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lahiru/travelpackageapi/TravelPackageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lahiru.travelpackageapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelPackageApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
